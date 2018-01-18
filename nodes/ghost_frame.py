83f9a2773ec4df7421829937c9d6ec39e6a88e3f
Replace Your Name Here <replace.your.email.here@gmail.com>
2018-01-18 11:55:15 -0300

    added more arucos

--- nodes/ghost_frame.py
+++ nodes/ghost_frame.py
@@ -8,10 +8,11 @@ if __name__ == '__main__':
     rospy.init_node('ghost_frame_spawner')  
     listener = tf.TransformListener()
     br = tf.TransformBroadcaster()
-    #listener.waitForTransform('odom','base_footprint',rospy.Time.now(),rospy.Duration(3.0))
+    listener.waitForTransform('odom','base_footprint',rospy.Time(0),rospy.Duration(3.0))
     while True :
         try :
             (trans,rot) = listener.lookupTransform('odom', 'base_footprint', rospy.Time(0))
+            rospy.loginfo("transform ok")
             break
         except:
             rospy.logerr('not transform between odom and base_footprint')
