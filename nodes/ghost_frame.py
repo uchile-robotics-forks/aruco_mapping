b6f363998551b27f44da83bdce7cf623d35c21f3
Bender Devel <bender.devel@gmail.com>
2018-01-30 14:58:18 -0300

    no me acuerdo

--- nodes/ghost_frame.py
+++ nodes/ghost_frame.py
@@ -11,10 +11,10 @@ if __name__ == '__main__':
     listener = tf.TransformListener()
     br = tf.TransformBroadcaster()
     #listener.waitForTransform('odom','base_footprint',rospy.Time.now(),rospy.Duration(3.0))
-    listener.waitForTransform('odom','base_footprint',rospy.Time(0),rospy.Duration(3.0))
+    listener.waitForTransform('odom','camera_orb_position',rospy.Time(0),rospy.Duration(3.0))
     while not rospy.is_shutdown() :
         try :
-            (trans,rot) = listener.lookupTransform('odom', 'base_footprint', rospy.Time(0))
+            (trans,rot) = listener.lookupTransform('odom', 'camera_orb_position', rospy.Time(0))
             rospy.loginfo("transform ok")
             break
         except:
